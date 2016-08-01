649cff4641050eab4b0ab4767eebda0f987f050d
chuck <14jhwang@stu.edu.cn>
2016-08-02 00:29:02 +0800

    kruskal

--- Graph/MinTree.c
+++ Graph/MinTree.c
@@ -53,3 +53,44 @@ void MinTree_Prim(MGraph G)
         }
     }
 }
+
+
+// Kruskal
+
+int Find(int *parent, int f) {
+    while (parent[f] > 0) {
+        f = parent[f];
+    }
+
+    return f;
+}
+
+void MinTree_Prim(MGraph G)
+{
+    int i, n, m;
+    Edge edges[MAXEDGE];
+    int parent[MAXVEX];
+
+    for (i = 0; i < G.numVertexes; ++i) {
+        parent[i] = 0;
+    }
+
+    for (i = 0; i < G.numEdges; ++i) {
+        n = Find(parent, edges[i].begin);
+        m = Find(parent, edges[i].end);
+
+        if (n != m) {
+            parent[n] = m;
+            printf("(%d, %d) %d", edges[i].begin, edges[i].end, edges[i].weight);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
