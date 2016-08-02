1f06ae0ecddd40b2ff295b7cbf0436c1c131b148
chuck <14jhwang@stu.edu.cn>
2016-08-02 10:50:36 +0800

    kruskal

---
+++ Graph/ShortestPath.c
@@ -0,0 +1,47 @@
+//
+// Created by JunhaoWang on 8/2/16.
+//
+
+#define MAXVEX 9
+#define INFINITY 65535
+
+typedef int Patharc[MAXVEX]; // Index of SP (Shortest Path)
+typedef int ShortPathTable[MAXVEX]; // Sum of weight of each vertex
+
+void ShortestPath_Dijkatra(MGraph G, int V0, Patharc *P, ShortPathTable *D)
+{
+    int v, w, k, min;
+    int final[MAXVEX]; // final[w] = 1 means the shorest path (V0, Vw) is calculated
+
+    // Init
+    for (v = 0; v < G.numVertexes; ++v) {
+        final[v] = 0;   // Indicate that we have not found SP for all vertex
+        (*D)[v] = G.arc[V0][v];
+        (*P)[v] = 0;
+    }
+    (*D)[V0] = 0;
+    final[V0] = 1;
+
+    // Begin
+    for (v = 1; v < G.numVertexes; ++v) {
+        min = INFINITY;
+        for (w = 0; w < G.numVertexes; ++w) {
+            if (!final[w] && (*D)[w] < min) {
+                k = w;
+                min = (*D)[w];
+            }
+        }
+        final[k] = 1; // Found the nearest vertex
+    }
+
+    // Update the shortest path
+    for (w = 0; w < G.numVertexes; ++w) {
+        if (!final[w] && (min + G.arc[k][w] < (*D)[w])) {
+            (*D)[w] = min + G.arc[k][w];
+            (*P)[w] = k;
+        }
+    }
+}
+
+
+
